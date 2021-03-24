aa520e64aa7092c433e337cfb3bf202250a0a945
shehanblast <shihanbinuka@gmail.com>
2021-03-24 09:40:05 +0530

    Add answers for 5th Question

--- lab4/app.js
+++ lab4/app.js
@@ -37,3 +37,28 @@ readStream.on('data', data => {
 //Q5
 const http = require('http');
 
+//Q5 - b
+// http.createServer((req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// }).listen(3000);
+
+//Q5 -d
+http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    switch (req.method) {
+        case 'GET':
+            res.write('<h1>Hello World</h1>');
+            res.end();
+            break;
+        case 'POST':
+            req.on('data', data => {
+                res.write('<h1>Hello ' + data + '</h1>');
+                res.end();
+            });
+            break;
+    }
+}).listen(3000, (err) => {
+    console.log('Server is listening to port 3000')
+});
