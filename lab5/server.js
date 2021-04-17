c6a5e49aefe3baab531890ef5afd9715eb204744
Binuka Sihan <51001664+shehanblast@users.noreply.github.com>
2021-04-17 12:24:18 +0530

    Add Post Router paths

--- lab5/server.js
+++ lab5/server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const HomeRoutes = require('./routes/home.router');
+const PostRoutes = require('./routes/post.router');
 const ApiRoutes = require('./api/posts.api');
 const bodyParser = require('koa-bodyparser');
 
@@ -8,15 +9,17 @@ const app = new Koa();
 // app.use(ctx => {
 //     ctx.body = 'Hello World';
 // });
+
+// app.listen(3000);
+
 app.use(bodyParser());
 
 app.use(HomeRoutes.routes())
     .use(HomeRoutes.allowedMethods());
 
-app.use(ApiRoutes.routes())
-    .use(ApiRoutes.allowedMethods());
+app.use(PostRoutes.routes())
+    .use(PostRoutes.allowedMethods());
 
 app.listen(3000);
 
 console.log('Application is running on port 3000');
-
